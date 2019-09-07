643b8ff55a05b65576bc64ad403d2004569309d8
Jack Cole <jackdcole@gmail.com>
2019-09-07 00:09:55 -0400

    Added Deck and Hand classes

--- app/src/main/java/app/jackdcole/deck/Deck.java
+++ app/src/main/java/app/jackdcole/deck/Deck.java
@@ -2,7 +2,23 @@ package app.jackdcole.deck;
 
 public class Deck extends Card {
 
-    public Deck() {
+    public final int DECK_SIZE = 52;
 
+    public enum Suit {
+        CLUBS, SPADES, DIAMONDS, HEARTS
+    }
+
+    public Deck() {
+        createDeck();
+    }
+
+    public Card[] createDeck() {
+        Card[] deck = new Card[DECK_SIZE];
+
+        return deck;
+    }
+
+    public Card[] shuffle(Card[] deck) {
+        return deck;
     }
 }
--- app/src/main/java/app/jackdcole/deck/Hand.java
+++ app/src/main/java/app/jackdcole/deck/Hand.java
@@ -2,6 +2,8 @@ package app.jackdcole.deck;
 
 public class Hand extends Card {
 
+    public final int HAND_SIZE = 2;
+
     public Hand() {
 
     }
