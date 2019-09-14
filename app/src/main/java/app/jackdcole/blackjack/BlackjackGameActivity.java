c931c8bd6c4806a8b1761d00f5f62acfad51e285
Jack Cole <jackdcole@gmail.com>
2019-09-14 15:32:14 -0400

    Added Buttons

---
+++ app/src/main/java/app/jackdcole/blackjack/BlackjackGameActivity.java
@@ -0,0 +1,35 @@
+package app.jackdcole.blackjack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class BlackjackGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blackjack_game);
+    }
+
+    public void standButton(View view) {
+
+    }
+
+    public void hitButton(View view) {
+
+    }
+
+    public void splitButton(View view) {
+
+    }
+
+    public void doubleDownButton(View view) {
+
+    }
+
+    public void surrenderButton(View view) {
+
+    }
+}
